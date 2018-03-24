a5a188caf7801db38f351d7f7b7479fa0bb2601d
Beatrice <csillig.beatrice@gmail.com>
2018-03-24 17:14:22 +0200

    adaugare navigatie

--- index.php
+++ index.php
@@ -1,19 +1,48 @@
 <?php
 include_once 'dbconfig.php';
-
+// include_once 'search.php';
 ?>
 <!DOCTYPE>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Bel</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<link rel="stylesheet" href="custom.css" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <center>
+    <nav>
+        <ul>
+            <li><a href="#">All</a></li>
+            <li><a href="#">Albania</a></li>
+            <li><a href="#">Bosnia and Herzegovina</a></li>
+            <li><a href="#">Bulgaria</a></li>
+            <li><a href="#">Croatia</a></li>
+            <li><a href="#">Hungary</a></li>
+            <li><a href="#">Kosovo</a></li>
+            <li><a href="#">Macedonia</a></li>
+            <li><a href="#">Moldova</a></li>
+            <li><a href="#">Montenegro</a></li>
+            <li><a href="#">Romania</a></li>
+            <li><a href="#">Serbia</a></li>
+            <li><a href="#">Ukraine</a></li>    
+        </ul>
+    </nav>
+    <header>
+        <div class="hottopics"><p><bold>H</bold>ot topic</p></div>
+        <img src="" class="logo"/>
+        <!-- <button type="button">Search...<?php ?></button> -->
+    </header>
 
 <div id="body">
  <div id="content">
-    <table align="center">
+    <form action="search.php" method="GET">
+        <input type="text" name="query" placeholder="Search..." />
+        <!-- <input type="submit" value="Search" /> -->
+    </form>
+
+    <!-- <table align="center">
     <tr>
     <th colspan="7"><a href="add_data.php">Add data here.</a></th>
     </tr>
@@ -44,7 +73,7 @@ include_once 'dbconfig.php';
         <?php
  }
  ?>
-    </table>
+    </table> -->
     </div>
 </div>
 
---
+++ search.php
@@ -0,0 +1,8 @@
+<!-- <?php
+	include 'dbconfig.php';
+	// $query = "SELECT * FROM tari";
+	$result = mysqli_query($con,"SELECT * FROM tari");	while ($row = $result->fetch_assoc())
+{
+    foreach($row as $value) echo "<td>$value</td>";
+	 }
+?> -->
